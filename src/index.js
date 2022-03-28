2bbce4fbba62fab3b26952603ac5b7c13e06bd11
Jhon Deiber Arcila Castaño <1053860350@UNO27.COM>
2022-03-28 15:03:11 -0500

    config babel and port

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import app from "./app";
 import http from "http";
 import socket from "./socket";
+import { PORT } from "./config";
 import { Server as WebSocketServer } from "socket.io";
 
 import { connectDB } from "./db";
 connectDB();
 
 const server = http.createServer(app)
-const httpServer = server.listen(3000);
+const httpServer = server.listen(PORT);
 const io = new WebSocketServer(httpServer);
 
 socket(io);
 
-console.log("Server is running on port 3000");
+console.log("Server is running on port " + PORT);
